96a0aba85e5a6384ccb060a22a180c4f7384f354
kadek surya mahardika <kadeksuryam@gmail.com>
2021-07-06 14:30:59 +0700

    finish all doctors endpoint handler

--- controllers/doctors.js
+++ controllers/doctors.js
@@ -1,8 +1,9 @@
 const doctorsRouter = require('express').Router()
 const Doctor = require('../models/doctor')
+const { authUser, authAdmin } = require('../utils/middleware')
 
 //CREATE A DOCTOR
-doctorsRouter.post('/', async (req, res, next) => {
+doctorsRouter.post('/', [authUser, authAdmin], async (req, res, next) => {
     const { name, desc } = req.body
 
     try{
@@ -18,6 +19,30 @@ doctorsRouter.post('/', async (req, res, next) => {
         next(err)
     }
 
+})
+
+//GET/READ ALL DOCTOR
+doctorsRouter.get('/', async (req, res, next) => {
+    try{
+        const doctors = await Doctor.find({})
+
+        return res.status(200).json(doctors)
+    } catch(err){
+        next(err)
+    }
+})
+
+//GET/READ ONE DOCTOR
+doctorsRouter.get('/:id', async (req, res, next) => {
+    try{
+        const doctorID = req.params.id
+        const doctor = await Doctor.findById(doctorID)
+
+        return res.status(200).json(doctor)
+        
+    } catch(err){
+        next(err)
+    }
 })
 
 module.exports = doctorsRouter
