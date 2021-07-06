63d3e3befb8e7e5c2a2c51b85acf029636dd686e
kadek surya mahardika <kadeksuryam@gmail.com>
2021-07-06 14:36:25 +0700

    add endpoints

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const patientsRouter = require('./controllers/patients')
 const doctorsRouter = require('./controllers/doctors')
 const appointmentsRouter = require('./controllers/appointments')
 const adminsRouter = require('./controllers/admins')
+const loginRouter = require('./controllers/login')
+const patientAppointmentsRouter = require('./controllers/patientAppointments')
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -23,11 +25,14 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
 app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
+app.use(middleware.tokenExtractor)
 
 //main endpoints
+app.use('/api/login', loginRouter)
 app.use('/api/patients', patientsRouter)
 app.use('/api/doctors', doctorsRouter)
 app.use('/api/appointments', appointmentsRouter)
+app.use('/api/patient-appointments', patientAppointmentsRouter)
 app.use('/api/admins', adminsRouter)
 
 app.use(middleware.unknownEndpoint)
